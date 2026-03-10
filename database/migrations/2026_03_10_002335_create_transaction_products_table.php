3b8c10b64cae8c61df446b1089ca1187619450b7
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-03-09 22:11:19 -0300

    feat: update transaction_products migration

--- database/migrations/2026_03_10_002335_create_transaction_products_table.php
+++ database/migrations/2026_03_10_002335_create_transaction_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('transaction_products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('transaction_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
