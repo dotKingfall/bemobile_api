a401552686f1649c9ace5f6ea9d92b6ea4dc972e
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-03-09 21:51:36 -0300

    feat: update gateways migration

--- database/migrations/2026_03_10_002133_create_gateways_table.php
+++ database/migrations/2026_03_10_002133_create_gateways_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('gateways', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('is_active')->default(true);
+            $table->integer('priority');
             $table->timestamps();
         });
     }
