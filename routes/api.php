f54e4e600d57a3ce7c9f7888fb9cb8510ff44eb3
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-03-11 00:18:34 -0300

    feat: add purchase route

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TransactionController;
 
+//PUBLIC ROUTES
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/buy', [TransactionController::class, 'store']);
 
 //PROTECTED ROUTES
 Route::middleware('auth:sanctum')->group(function () {
