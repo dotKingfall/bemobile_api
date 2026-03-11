5e61bb0b19fca0244a52bf43537cc0079b81e373
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-03-11 02:57:13 -0300

    feat: transaction controller custom messages

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -11,10 +11,13 @@ class TransactionController extends Controller
             //FIELDS
             'product_id' => 'required|exists:products,id',
             'name' => 'required|string|min:1|max:255',
+            'email'      => 'required|email',
             'quantity' => 'nullable|integer|min:1',
         ], [
             //CUSTOM MESSAGES
             'name.required' => 'The customer name cannot be empty.',
+            'email.required' => 'The customer email cannot be empty.',
+            'email.email' => 'The customer email is not valid.',
         ]);
 
         //REMOVE EVERYTHING BUT BLANK SPACES, NUMBERS AND LETTERS FROM
