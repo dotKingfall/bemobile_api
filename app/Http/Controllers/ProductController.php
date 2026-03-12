b208cd582a32d97cacc4c5caa588aefd33d3950e
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-03-12 12:18:25 -0300

    feat: products CRUD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,16 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
-    public function index(){}
+    public function index(){
+        //I MEAN, I THINK 50 IS A GOOD NUMBER :D
+        return response()->json(Product::paginate(50));
+    }
 
-    public function store(Request $request){}
+    public function store(Request $request){
+        $validated = $request->validate([
+            'name'   => 'required|string|max:255',
+            'amount' => 'required|integer|min:0',
+        ]);
 
-    public function show(Product $product){}
+        Log::info('Creating product', ['name' => $validated['name']]);
 
-    public function update(Request $request, Product $product){}
+        $product = Product::create($validated);
 
-    public function destroy(Product $product){}
+        Log::info('Product ' . $product->name . ' created', ['id' => $product->id]);
+        return response()->json($product, 201);
+    }
+
+    public function show(Product $product){
+        return response()->json($product);
+    }
+
+    public function update(Request $request, Product $product){
+        $validated = $request->validate([
+            'name'   => 'string|max:255',
+            'amount' => 'integer|min:0',
+        ]);
+
+        $product->update($validated);
+        Log::info('Product ' . $product->name . ' updated', ['id' => $product->id]);
+
+        return response()->json($product);
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully'], 204);
+    }
 }
